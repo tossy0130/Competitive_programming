728558815bc8a8bb60acb2aaaac95e557a85ebd7
tossy0130 <tokotoko33ok@gmail.com>
2022-10-07 16:49:56 +0900

    2022_10_07_c#_02

--- C#/01.cs
+++ C#/01.cs
@@ -1,9 +1,36 @@
 using System;
 
 class Program
-{
+{   
+    
+    /*
+    ある数をある回数表示 1
+    3 を 8 回、改行区切りで出力
+    */
+     public void t_01(int num, int loop_num){
+        
+        for(var i = 0; i < loop_num; i++) {
+            Console.WriteLine(num.ToString());
+        }
+     }
+
+    /*
+        入力例1
+        3 8
+    */
+    public void t_02(int num, int loop_num){
+        
+        for(var i = 0; i < loop_num; i++) {
+            Console.WriteLine(num.ToString());
+        }
+        
+    }
+
     static void Main()
     {
+
+         Program obj = new Program();
+
         // 自分の得意な言語で
         // Let's チャレンジ！！
         for(var i = 1; i <= 10; i++) {
@@ -26,7 +53,40 @@ class Program
           Console.WriteLine(val);
        }
 
+        /*
+        入力例1
+        5
+        1 2 3 4 5
+        */
+        
+         var num = int.Parse(Console.ReadLine());
+         var arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+         
+         foreach(var val in arr) {
+             Console.WriteLine(val);
+         }
 
+         //  ある数をある回数表示 1 function t_01
+         obj.t_01(3, 8);
+
+        /*
+        入力例1
+        3 8
+        */
+        obj.t_02(arr[0], arr[1]);
+
+
+        /*
+          a ~ z までを表示
+        */
+        //=== a ~ z までを表示
+        string[] texts_arr = new string[] { "a", "b", "c" ,"d","e","f","g","h","i","j","k","l","m","n"
+                                    ,"o","p","q","r","s","t","u","v","w","x","y","z"};
+                                    
+        foreach(var val in texts_arr) {
+            Console.WriteLine(val);
+        }
+        // === a ~ z までを表示 END
 
     }
 }
